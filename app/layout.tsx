12496a9a4e9e58c749505d85b32426486afcdef0
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-17 21:48:32 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,7 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <link rel="icon" sizes="32x32" href="/favicon.ico" />
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
