700f3cf85e235a1e56fa019298f47dee760b9c71
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-17 21:17:17 +0900

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -22,7 +22,18 @@ export function Navbar() {
         >
           <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
-              return (
+              const isExternal = path.startsWith('http');
+              return isExternal ? (
+                <a
+                  key={path}
+                  href={path}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                >
+                  {name}
+                </a>
+              ) : (
                 <Link
                   key={path}
                   href={path}
