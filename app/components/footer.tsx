29b548822dfe3112bae8e008541612274435ddc8
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-17 21:10:25 +0900

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -30,6 +30,17 @@ export default function Footer() {
             <p className="ml-2 h-7">github</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.notion.so/hpicjh/02ceb5faca224d1f97fc502711fe7cfe"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">notion</p>
+          </a>
+        </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} MIT Licensed
