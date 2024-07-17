9ff8e2e9bf6830c395234c1838a4f06d12e29a04
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-17 20:32:55 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,25 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         소개
       </h1>
-      <h3 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h3 className="mb-8 font-semibold tracking-tighter">
       Research Interests
       </h3>
       <p className="mb-4">
         {`AI hardware design and Digital PIM`}
       </p>
-      <h3 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h3 className="mb-8 font-semibold tracking-tighter">
       Tape-out Schedule
       </h3>
       <p className="mb-4">
         {``}
       </p>
-      <h3 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h3 className="mb-8 font-semibold tracking-tighter">
       Details
       </h3>
       <p className="mb-4">
         {`Currently pursuing a Bachelor's degree in the school of electrical engineering at Hanyang University, Ansan, South Korea. His research interests include artificial intelligence hardware design and Digital processing in memory (Digital PIM).`}
       </p>
-      <h3 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h3 className="mb-8 font-semibold tracking-tighter">
       Email
       </h3>
       <p className="mb-4">
