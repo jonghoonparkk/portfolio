7f9e3ce78478bef4c7b55d10b82e0eeaaaa0a1fd
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-18 16:04:17 +0900

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -42,9 +42,6 @@ export default function Footer() {
           </a>
         </li>
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
-      </p>
     </footer>
   )
 }
